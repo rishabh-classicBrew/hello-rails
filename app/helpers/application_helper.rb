c725d4ae720e28c1fe21005e4b60ea1e17ad0d21
rishabh-classicBrew <rishabh.pagaria@tamu.edu>
2024-10-09 07:10:14 -0500

    adding sorting color functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
     def sortable(column, title = nil)
       title ||= column.titleize
       direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-      link_to title, { sort: column, direction: direction }
+      css_class = column == params[:sort] ? "current #{params[:direction]}" : nil
+      direction_symbol = column == params[:sort] ? (params[:direction] == "asc" ? " ↑" : " ↓") : ""
+      link_to "#{title}#{direction_symbol}".html_safe, { sort: column, direction: direction }, { class: css_class }
     end
   end
